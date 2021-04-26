ef2efe76c696873c488041e9753338a70254ac4d
Ventona <defreitas.tony.pro@gmail.com>
2021-04-26 23:31:36 +0200

    Modification de l'interface et correction de bug

--- cheaterTransaction.c
+++ cheaterTransaction.c
@@ -3,41 +3,41 @@
 //
 
 #include "cheaterTransaction.h"
-#include <stdlib.h>
 #include "merkleTree.h"
 #include <stdio.h>
 #include <time.h>
 #include "Minage/mining.h"
-#include "merkleTree.h"
 
-
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define RESET "\x1B[0m"
 
 void deleteTransaction(Blockchain *blockchain, int blockIndex, int txIndex) {
     clock_t t1=clock();
     double time_spent=0.0;
-    mining(blockchain->blocks[blockIndex]); //TODO, sert un peu à rien
-    //printf("Previous hash : %s\n",blockchain->blocks[blockIndex]->hashCodePredecessor);
-    printf("Hash du block %d avant suppresion de la transaction %d : %s\n",blockIndex,txIndex,blockchain->blocks[blockIndex]->hashCode);
-    merkleTree(blockchain->blocks[blockIndex]); //TODO, sert un peu à rien
-    printf("Merkle root avant suppression de la transaction : %s\n", blockchain->blocks[blockIndex]->hashMerkleTreeRoot);
-    blockchain->blocks[blockIndex]->txList->tx[txIndex]=""; //TODO, decaler les transactions
-    blockchain->blocks[blockIndex]->txList->txNumber--; //TODO, same
-    //TODO, free la dernière transaction après décalage de toutes les transactions
-    merkleTree(blockchain->blocks[blockIndex]);
-    printf("Merkle root apres suppression de la transaction : %s\n", blockchain->blocks[blockIndex]->hashMerkleTreeRoot);
 
-    for (int i = blockIndex; i < blockchain->blockCursor; ++i) {
+    printf(RED "[Cheater de transaction]" RESET " - Suppression de la transaction %d du block %d.\n", txIndex, blockIndex);
+    printf("Operation en cours ...\n");
+
+    int txNumber = blockchain->blocks[blockIndex]->txList->txNumber;
+    for (int i = txIndex; i < txNumber - 1; i++){
+        blockchain->blocks[blockIndex]->txList->tx[i] = blockchain->blocks[blockIndex]->txList->tx[i+1];
+    }
+    blockchain->blocks[blockIndex]->txList->tx[txNumber - 1] = NULL;
+    blockchain->blocks[blockIndex]->txList->txNumber--;
+
+    merkleTree(blockchain->blocks[blockIndex]);
+
+    for (int i = blockIndex; i > 0 && i < blockchain->blockCursor; ++i) {
         blockchain->blocks[i]->nonce=0;
-        strcpy(blockchain->blocks[i]->hashCodePredecessor,blockchain->blocks[i-1]->hashCode); //TODO, si c'est le genesis ca va planter
+        strcpy(blockchain->blocks[i]->hashCodePredecessor,blockchain->blocks[i-1]->hashCode);
         mining(blockchain->blocks[i]);
-        printf("Hash du block %d: %s\n",i, blockchain->blocks[i]->hashCode);
     }
 
     clock_t t2=clock();
     time_spent += (double)(t2- t1) / CLOCKS_PER_SEC;
-    //printf("Previous hash : %s\n",blockchain->blocks[blockIndex]->hashCodePredecessor);
-   // printf("Previous hash : %s\n",blockchain->blocks[blockIndex+1]->hashCodePredecessor);
-    printf("Temps d'execution : %f secondes\n",time_spent);
+    printf(GRN "[Cheater de transaction] - Operation terminee\n");
+    printf(RESET "Temps d'execution : %f seconde(s)\n", time_spent);
 }
 
 
