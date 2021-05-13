faadccc5698c95231861c226b8a30b07c433dd54
fourmi <clementfourmii@hotmail.fr>
2021-05-13 14:11:45 +0200

    Ouverture de la fenetre d'interface au milieu de l'écran

--- src/Tools/CreateGui.java
+++ src/Tools/CreateGui.java
@@ -4,6 +4,7 @@ import BlockChain.BlockChain;
 
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.awt.*;
 
 public class CreateGui {
     private int difficulte;
@@ -18,7 +19,11 @@ public class CreateGui {
         this.guiBC = new BCGui();
         guiBC.setTitle("BlockChain");
         guiBC.pack();
-        guiBC.setLocale(null);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        int x = (screenSize.width - guiBC.getWidth()) / 2;
+        int y = (screenSize.height - guiBC.getHeight()) / 2;
+        guiBC.setLocation(x, y); //Met la fenêtre au milieu de l'écran
         guiBC.setSize(700,225);
         guiBC.setVisible(true);
         guiBC.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
