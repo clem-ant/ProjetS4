17be6e4edbfbd097bbad468c92115048790a53be
fourmi <clementfourmii@hotmail.fr>
2021-05-13 15:21:33 +0200

    Creation des users maintenant avec le creator, mineurs et users

--- src/BlockChain/BlockChain.java
+++ src/BlockChain/BlockChain.java
@@ -140,21 +140,7 @@ public class BlockChain {
         //TODO faire perdre et gagner de l'argent a la personne qui donne et celle qui reçoit
     }
 
-    public User[] createNUsers(int nbUsers){
-        User[] users = new User[nbUsers];
-        int nom, type;
-        ArrayList<String> listePrenoms = new ArrayList<>(Arrays.asList("joyce","lola","brian","louis", "an", "charles","diane","ravi","achille","nathan","javier","chance","lois","dominique","pedro","karl","rose","james","constance","roland","christine","archibald","renard","angel","mary","sam","stepan","eugenio","eden","justice","bart","alix","guy","mo","alfred","francine","brigitte","iris","eddie","otto","fabia","roger","maurice","louise","camille","luis","sabine","chantal","val","paulo","lin","giselle","caroline","sandra","trace","michel","andré","pauline","jung","colombe","lou","miguel","francisco","gladys","edgar","rodrigo","oscar","young","ion","carl","li","edward","alex","fernando","avril","albert","temple","catherine","lourdes","sage","marin","douglas","gordon","jacqueline","colin","paco","julius","josé","scott","roy","felipe","ariane","marek","denise","london","taylor","hermine","philip","indigo","malvina"));
-        for(int i = 0; i < nbUsers; i++){
-            type = RandomNumber.getRandomNumberInRange(0,1);
-            nom = RandomNumber.getRandomNumberInRange(0,nbUsers-1);
-            if(type == 0){
-                users[i] = new User(listePrenoms.get(nom), listePrenoms.get(nom), 0);
-            }else{
-                users[i] = new Mineur(listePrenoms.get(nom), listePrenoms.get(nom), 0);
-            }
-        }
-        return users;
-    }
+
 
     public void remplirBC(User[] users){
         for(int i = 1; i < nbBlock; i++){
--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,31 @@ import Utilisateurs.Creator;
 import Utilisateurs.Mineur;
 import Utilisateurs.User;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 /**
  * The type Main.
  */
 public class Main {
 
+    public static User[] createNUsers(int nbUsers){
+        User[] users = new User[nbUsers];
+        int nom, type;
+        ArrayList<String> listePrenoms = new ArrayList<>(Arrays.asList("joyce","lola","brian","louis", "an", "charles","diane","ravi","achille","nathan","javier","chance","lois","dominique","pedro","karl","rose","james","constance","roland","christine","archibald","renard","angel","mary","sam","stepan","eugenio","eden","justice","bart","alix","guy","mo","alfred","francine","brigitte","iris","eddie","otto","fabia","roger","maurice","louise","camille","luis","sabine","chantal","val","paulo","lin","giselle","caroline","sandra","trace","michel","andré","pauline","jung","colombe","lou","miguel","francisco","gladys","edgar","rodrigo","oscar","young","ion","carl","li","edward","alex","fernando","avril","albert","temple","catherine","lourdes","sage","marin","douglas","gordon","jacqueline","colin","paco","julius","josé","scott","roy","felipe","ariane","marek","denise","london","taylor","hermine","philip","indigo","malvina"));
+        users[0] = new Creator(listePrenoms.get(RandomNumber.getRandomNumberInRange(0,1)), listePrenoms.get(RandomNumber.getRandomNumberInRange(0,1)), 0);
+        for(int i = 1; i < nbUsers; i++){
+            type = RandomNumber.getRandomNumberInRange(0,1);
+            nom = RandomNumber.getRandomNumberInRange(0,nbUsers-1);
+            if(type == 0){
+                users[i] = new User(listePrenoms.get(nom), listePrenoms.get(nom), 0);
+            }else{
+                users[i] = new Mineur(listePrenoms.get(nom), listePrenoms.get(nom), 0);
+            }
+        }
+        return users;
+    }
     /**
      * Main.
      *
@@ -23,11 +42,11 @@ public class Main {
         int nbBlock = guiBC.getNbBlock();
         int nbMaxTransac = guiBC.getNbMaxTransac();
 
-
-        Creator simrun = new Creator("Simran", "Jesaispasencore", 50);
+        User[] users = createNUsers(100);
+        Creator simrun = (Creator) users[0];
         BlockChain blockChain = new BlockChain(difficulte, nbBlock, simrun, nbMaxTransac);
         simrun.recevoirBnb(50); //Recompense pour avoir créer le genesis par la coinbase
-        User[] users = blockChain.createNUsers(100);
+
         for(int i = 0; i < 100; i++){
             users[i].recevoirBnb(50); //Helicopter money
         }
